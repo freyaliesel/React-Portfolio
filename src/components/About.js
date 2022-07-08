d6b253530a971e6ad1e882a880bb5ac6eb878d6a
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-07-08 15:39:56 -0500

    ported about me section

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,44 @@
 function About() {
     return (
-        <div>About info</div>
-    )
+        <main>
+            <section id="aboutme">
+                <h2>About Me</h2>
+                <div className="container">
+                    <img
+                        id="profileimage"
+                        src="./assets/images/profileimage.png"
+                        alt="picture of Ms Glidden, smiling"
+                    />
+                    <p>
+                        Hi I'm Kit, and I'm an aspiring developer! I've spent my
+                        life on computers, and it only makes sense to me that I
+                        would take a crack at making a living programming
+                        applications for them. I'm taking Northwestern
+                        University's Coding Bootcamp in an effort to make a
+                        career change.
+                    </p>
+                    <p>
+                        My professional history has seen me spend nearly two
+                        decades in roles for various food service and retail
+                        employers. During this time, I've volunteered in
+                        managerial capacities for several non-profits and
+                        religious organizations. Additionally, I own and
+                        administrate a Discord server for a community comprising
+                        over 17,000 members.
+                    </p>
+                    <p>
+                        On a more personal level, I'm originally from Fairfax,
+                        Virginia. I currently live in Chicago with my 11 year
+                        old Sphynx cat, William, and he likes to yell at me
+                        loudly while I do homework or try to relax. I spend my
+                        free time playing video games, watching tv series with
+                        my friends in a club we've had going for nearly two
+                        years, and I recently started a Gloomhaven campaign!
+                    </p>
+                </div>
+            </section>
+        </main>
+    );
 }
 
-
-export default About
+export default About;
