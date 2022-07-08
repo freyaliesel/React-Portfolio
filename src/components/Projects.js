115c3eab3cccf5bb8f30f82e9b55e4d6f65b4edf
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-07-08 14:52:05 -0500

    added projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,72 @@
+import { Link } from "react-router-dom";
+
 function Projects() {
     return (
-        <div>Projects go here</div>
-    )
+        <section id="work">
+            <h2>Work</h2>
+            <div className="container">
+                <Link id="dately" className="heroproject project" to="#">
+                    <div id="herocontent" className="content">
+                        Dately
+                        <span className="language">Javascript/HTML/CSS</span>
+                    </div>
+                    <img
+                        src="./assets/images/Dately.png"
+                        alt="preview image of the web app Dately"
+                    />
+                </Link>
+                <Link id="manyfest" className="project" to="">
+                    <div className="content">
+                        Manyfest
+                        <span className="language">Javascript/Handlebars</span>
+                    </div>
+                    <img
+                        src="./assets/images/manyfest.png"
+                        alt="preview image of a game made in scratch"
+                    />
+                </Link>
+                <Link id="weather" className="project" to="">
+                    <div className="content">
+                        {" "}
+                        Weather
+                        <span className="language">Javascript/HTML/CSS</span>
+                    </div>
+                    <img
+                        src="./assets/images/weather-dashboard.png"
+                        alt="preview image of a weather dashboard web app"
+                    />
+                </Link>
+                <Link id="scheduler" className="project" to="">
+                    <div className="content">
+                        Scheduler
+                        <span className="language">Javascript/HTML</span>
+                    </div>
+                    <img
+                        src="./assets/images/worday-scheduler.png"
+                        alt="preview image of a worday schedule web app"
+                    />
+                </Link>
+                <Link id="quiz" className="project" to="">
+                    <div className="content">
+                        Code Quiz<span className="language">Javascript</span>
+                    </div>
+
+                    <img
+                        src="./assets/images/code-quiz.png"
+                        alt="placeholder image from picsum"
+                    />
+                </Link>
+
+                {/* <!-- <a id="scratch" className="project" href=""
+            >
+            <div className="content">Memory<span className="language">Scratch</span></div>
+            <img
+              src="./assets/images/memorygame.png"
+              alt="preview image of a game made in scratch"
+          /></a> --> */}
+            </div>
+        </section>
+    );
 }
 
-
-export default Projects
+export default Projects;
