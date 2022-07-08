46e355b6451816a7b25bcf2fd161c7b900128a15
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-07-08 16:03:20 -0500

    added contact links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
     return (
         <footer>
-                <h3>©2022 K Glidden</h3>
+            <h3>
+                ©2022 K Glidden <br />
+                <Link to="https://www.linkedin.com/in/kelseaglidden/" >
+                    <i class="fa-brands fa-linkedin"></i>
+                </Link>
+                <Link to="https://github.com/freyaliesel"><i class="fa-brands fa-github"></i></Link>
+                <Link to="mailto:freyacodes@gmail.com"><i class="fa-solid fa-envelope"></i></Link>
+                <Link to="tel:+13127678068"><i class="fa-solid fa-phone"></i></Link>
+            </h3>
         </footer>
-    )
+    );
 }
 
-
-export default Footer
+export default Footer;
