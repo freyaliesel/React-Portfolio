86abf6dd9e92c9b1106f044327f2689f2648fbac
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-07-08 14:45:24 -0500

    added contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,29 @@
 function Contact() {
     return (
-        <div>Contact info</div>
-    )
+        <section id="contactme">
+            <h2>Contact Me</h2>
+            <div className="container">
+                <ul>
+                    <li>
+                        <a href="tel:+13127678068">1·312·767·8068</a>
+                    </li>
+                    <li>
+                        <a href="mailto:freyacodes@gmail.com">
+                            freyacodes@gmail.com
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/freyaliesel">GitHub</a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/kelsea-glidden-907490126/">
+                            LinkedIn
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    );
 }
 
-
-export default Contact
+export default Contact;
